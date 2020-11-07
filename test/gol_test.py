e8d66e0685a0ef50a040bd2c1e8a4c6ea79afcb9
renkai <renkai@soptai.sg>
2020-11-07 12:34:17 +0800

    script and test

---
+++ test/gol_test.py
@@ -0,0 +1,61 @@
+import pytest
+from gol.gol import * #folder is called "gol" as well, so we have to do the gol.gol thing :D
+
+
+class TestUnderpop():
+    def test_underpop_positive(self):
+        '''
+        Positive case to determine an underpopulation event
+        '''
+        testArray = [[0,1,0],[0,1,0],[0,0,0]]
+        assert underpop(testArray,1,1) == True
+    
+    def test_underpop_negative(self):
+        '''
+        Negative case to test underpopulation
+        '''
+        #This one im not doing right
+        testArray = [[0,1,0],[1,1,0],[0,0,0]]
+        assert underpop(testArray,1,1) == False
+
+class TestOverpop():
+    def test_overpop_positive(self):
+        '''
+        Testing if we are detecting the overpopulation right
+        '''
+        #This one doesnt work
+        testArray = [[1,1,0],[1,1,0],[0,0,0]]
+        assert overpop(testArray,1,1) == True
+
+    def test_overpop_negative(self):
+        '''
+        Testing if we can detect if theres no overpopulation
+        '''
+        testArray = [[0,1,0],[0,1,0],[0,0,0]]
+        assert overpop(testArray,1,1) == False
+
+class TestRepro():
+    def test_repro_positive(self):
+        '''
+        expect True if reproduction is expected
+        '''
+        #not working
+        testArray = [[0, 1, 0],
+                     [1, 0, 1],
+                     [0, 1, 0]]
+        assert repro(testArray, 1, 1) == True
+    
+
+    def test_repo_negative(self):
+        '''
+        expect False if reproduction is not expected
+        '''
+        testArray = [[0, 1, 0],
+                     [1, 0, 0],
+                     [0, 1, 0]]
+        assert repro(testArray, 1, 1) == False
+
+
+if __name__ == '__main__':
+    '''
+    '''
